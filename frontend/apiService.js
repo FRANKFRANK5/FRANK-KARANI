b192adf3bbadfc49bc3da42fb91798c4ac2c5dc5
FRANKFRANK5 <frankkarani146@gmail.com>
2026-05-21 09:58:44 +0300

    convert api url to render online

--- frontend/apiService.js
+++ frontend/apiService.js
@@ -1,5 +1,6 @@
 // apiService.js
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://onrender.com';
+
 
 /**
  * Mbinu ya usaidizi wa ndani kupata Token ya Admin kutoka localStorage
