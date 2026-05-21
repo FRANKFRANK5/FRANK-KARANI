220843929acaef2bfa096366d03182738118113a
FRANKFRANK5 <frankkarani146@gmail.com>
2026-05-21 15:10:25 +0300

    fix asset paths for render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // <--- Huu mstari ni muhimu sana kwa ajili ya Render!
   build: {
     outDir: 'dist'
   }
